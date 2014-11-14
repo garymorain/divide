f871525f7c10ecca2e6ca2389fb5b09265e509f0
Gary Morain <gmorain@google.com>
2014-11-14 14:04:00 -0800

    Test with remainders.

--- divide_test.c
+++ divide_test.c
@@ -7,20 +7,24 @@
 
 int main(int argc, char **argv) {
   bool test_fail = false;
-  for (uint32_t m1 = 1; m1 < (1 << 12); m1++) {
-    for (uint32_t m2 = 1; m2 < (1 << 12); m2++) {
-      uint32_t dividend = m1 * m2;
-      uint32_t divisor = m2;
-      uint32_t expected_quotient = m1;
-      uint32_t expected_remainder = 0;
-      uint32_t quotient = 0;
-      uint32_t remainder = 0;
-      divide(dividend, divisor, &quotient, &remainder);
-      if ((quotient != expected_quotient) || (remainder != expected_remainder)) {
-        test_fail = true;
-        printf("%u/%u.  Want %u, %u.  Got %u, %u\n",
-               dividend, divisor, expected_quotient, expected_remainder,
-               quotient, remainder);
+  for (uint32_t m1 = 1; m1 < (1 << 9); m1++) {
+    for (uint32_t m2 = m1 + 1; m2 < (1 << 9); m2++) {
+      for (uint32_t m3 = 1; m3 < m2; m3++) {
+        uint32_t dividend = m1 * m2 + m3;
+        uint32_t divisor = m2;
+        uint32_t expected_quotient = m1;
+        uint32_t expected_remainder = m3;
+        uint32_t quotient = 0;
+        uint32_t remainder = 0;
+        divide(dividend, divisor, &quotient, &remainder);
+        if ((quotient != expected_quotient) ||
+            (remainder != expected_remainder)) {
+          test_fail = true;
+          printf("%u/%u.  Want %u, %u.  Got %u, %u\n",
+                 dividend, divisor, expected_quotient,
+                 expected_remainder,
+                 quotient, remainder);
+        }
       }
     }
   }
